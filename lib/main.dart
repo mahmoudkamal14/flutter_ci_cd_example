781ad80eb4947cdb3aa36f010de08eff8b733848
Mahmoud Kamal <https://github.com/mahmoudkamalx14>
2025-06-24 12:40:28 +0300

    Test And Distribute

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_ci_cd_example/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
